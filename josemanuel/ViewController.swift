b003f9b653ef30f5a70cb65a87239d081ac2eb7e
José Manuel Castellano Domínguez <rpgdragon@joses-mac.local>
2015-11-03 11:37:53 +0100

    Initial Commit

---
+++ josemanuel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  josemanuel
+//
+//  Created by José Manuel Castellano Domínguez on 03/11/15.
+//  Copyright © 2015 José Manuel Castellano Domínguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
